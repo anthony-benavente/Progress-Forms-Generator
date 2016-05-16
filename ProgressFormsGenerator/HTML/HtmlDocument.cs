eee65192b6056d8c3037fddbd871a11c93324b47
Anthony Benavente <anthony.m.benavente@gmail.com>
2016-05-16 16:55:03 -0400

    Initial commit

---
+++ ProgressFormsGenerator/HTML/HtmlDocument.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressFormsGenerator.HTML
+{
+    public class HtmlDocument
+    {
+        private HtmlElement rootHtml;
+
+        public HtmlDocument()
+        {
+            rootHtml = new HtmlElement("html");
+        }
+    }
+}
---
+++ ProgressFormsGenerator/HTML/HtmlElement.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgressFormsGenerator.HTML
+{
+    public class HtmlElement
+    {
+        private string tagName;
+        private string innerHTML;
+        private Dictionary<string, string> attributes;
+        private HashSet<string> classes;
+        private List<HtmlElement> children;
+
+        public string TagName { get { return tagName; } set { tagName = value; } }
+        public string InnerHTML { get { return innerHTML; } set { innerHTML = value; } }
+        public Dictionary<string, string> Attributes { get { return attributes; } }
+        public List<HtmlElement> Children { get { return children; } }
+
+
+        public HtmlElement(string tagName)
+        {
+            this.tagName = tagName;
+            this.innerHTML = "";
+            this.attributes = new Dictionary<string, string>();
+            this.children = new List<HtmlElement>();
+            this.classes = new HashSet<string>();
+        }
+
+        public void AddChildren(params HtmlElement[] elements)
+        {
+            foreach (HtmlElement element in elements) 
+            {
+                children.Add(element);
+            }
+        }
+
+        public void AddAttribute(string attributeName, string attributeValue = "")
+        {
+            attributes.Add(attributeName, attributeValue);
+        }
+
+        public void AddClass(string className)
+        {
+            classes.Add(className);
+        }
+
+        public void RemoveClass(string className)
+        {
+            classes.Remove(className);
+        }
+
+        public override string ToString()
+        {
+            StringBuilder result = new StringBuilder("<" + tagName + " ");
+            foreach (var attr in attributes)
+            {
+                result.Append(String.Format("{0}=\"{1}\" ", attr.Key, attr.Value));
+            }
+            if (classes.Count > 0)
+            {
+                result.Append(String.Format("class=\"{0}\" ", String.Join(" ", classes)));
+            }
+            result.Append(">").Append(innerHTML);
+            foreach (HtmlElement child in children)
+            {
+                result.Append(child.ToString());
+            }
+            result.Append("</").Append(tagName).Append(">");
+            return result.ToString();
+        }
+    }
+}
